afee6962907a4e617b5bb4585b78a275b79cb5c6
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-08-12 21:47:34 +0700

    merge register, login dong bo voi front end

--- src/main/java/com/project/medium/controllers/AccountController.java
+++ src/main/java/com/project/medium/controllers/AccountController.java
@@ -20,7 +20,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("api/accounts")
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("*")
 public class AccountController {
 
     @Autowired
